7cdf95a55ece1af92f57a6413bece8c0260c5f3e
Gabor Lakatos <laagabinl@gmail.com>
2025-03-10 19:09:07 +0000

    Add script for installing and configuring Puppet Enterprise

---
+++ pe_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# configure locale support for en_US.UTF-8
+sudo locale-gen en_US.UTF-8 && sudo update-locale LANG=en_US.UTF-8
+
+# download and unpack the Puppet Enterprise tarball
+curl -JLO 'https://pm.puppet.com/cgi-bin/download.cgi?dist=ubuntu&rel=22.04&arch=amd64&ver=latest' && tar -xf tar -xf puppet-enterprise-2025.1.0-ubuntu-22.04-amd64.tar.gz
+
+# install Puppet Enterprise
+sudo ./puppet-enterprise-2025.1.0-ubuntu-22.04-amd64/puppet-enterprise-installer
+
+# ask the user to configure the password for the PE console
+echo "Please enter the password for the Puppet Enterprise console:"
+read -s PASSWORD
+
+sudo puppet infrastructure console_password --password=$PASSWORD
+
+# trigger puppet agent 2 times to ensure the node is fully configured
+sudo puppet agent -t 
+sudo puppet agent -t
